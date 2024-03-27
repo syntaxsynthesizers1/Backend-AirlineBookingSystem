71b8fc5f8f4c7e6046ac453dc7d30c905ad4332b
Timmy009 <timileyinbamgbose7@gmail.com>
2024-03-27 13:08:41 +0100

    Initial commit

---
+++ src/test/java/com/fourflyairline/backendairlinebookingsystem/BackendAirlineBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourflyairline.backendairlinebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAirlineBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
